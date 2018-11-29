27b381267f9a0737ababbbbfe7970e8b691a769c
Youri Arkesteijn <youri.essa@gmail.com>
2018-11-29 11:55:58 +0100

    Cleanup, plus data calculation added

--- datastructure.js
+++ datastructure.js
@@ -1,22 +1,3 @@
-var Collatz = (function () {
-    function Collatz() {
-    }
-    return Collatz;
-})();
-
-// class CollatzMap {
-//     private readonly mapping: Map<String, CollatzNode>;
-//     constructor() {
-//         this.mapping = new Map<String, CollatzNode>();
-//     }
-//     add(val: String, node: CollatzNode) {
-//         this.mapping.set(node.getValue(), node);
-//         return this;
-//     }
-//     get(val: String) {
-//         return this.mapping.get(val);
-//     }
-// }
 var mapping = new Map();
 
 var CollatzNode = (function () {
@@ -32,22 +13,6 @@ var CollatzNode = (function () {
     CollatzNode.prototype.getValue = function () {
         return this.value;
     };
-
-    CollatzNode.prototype.printDown = function (medium) {
-        medium.write(this.getValue());
-        if (this.previous) {
-            this.previous.printDown(medium);
-        }
-    };
-
-    CollatzNode.prototype.printUp = function (medium) {
-        medium.write(this.getValue());
-        this.next.forEach(function (x) {
-            medium.write("[");
-            x.printUp(medium);
-            medium.write("]");
-        });
-    };
     return CollatzNode;
 })();
 
@@ -55,9 +20,6 @@ function isEven(n) {
     return !(n & 1);
 }
 
-/**
-* @return Array of "step" values
-*/
 function getPrev(n) {
     if (n == 1) {
         return 1;
@@ -70,19 +32,6 @@ function getPrev(n) {
     }
 }
 
-// class StringMedium implements Medium {
-//     private buffer: String;
-//     constructor() {
-//         this.buffer = "";
-//     }
-//     write(value) {
-//         this.buffer = `${this.buffer} ${value}`;
-//     }
-//     read() {
-//         return this.buffer;
-//     }
-// }
-//class PrefixableStringMedium implements Medium
 mapping.set("1", new CollatzNode("1"));
 
 function addCollatz(i) {
@@ -98,36 +47,10 @@ function addCollatz(i) {
     return node;
 }
 
-// let medium = new StringMedium();
-// mapping.get("1").printUp(medium);
-// console.log(medium.read())
-addCollatz(20);
-addCollatz(3);
-addCollatz(13);
-addCollatz(80);
-addCollatz(17);
-addCollatz(104);
-
-// let medium = new StringMedium();
-// mapping.get("1").printUp(medium);
-// console.log(medium.read())
 console.dir(mapping.get("1"));
-// console.dir(mapping);
-// console.log("next::::");
-// medium = new StringMedium();
-// addCollatz(20).printUp(medium);
-// console.log(medium.read());
-// console.dir(mapping);
-// for(var i = 1; i < stop; i++){
-//     var v = i, v2;
-//     var node = new CollatzNode(i+""), node2, node3;
-//     node3 = node;
-//     do {
-//         mapping.set(v2+"", node2);
-//         v2 = getPrev(v);
-//         node2 = new CollatzNode(v2+"");
-//         node3.addToPrev(node2);
-//         node3 = node2;
-//     } while(!mapping.has(v2))
-//     node3.addToPrev(mapping.get(v2+""));
-//}
+
+var stop = 1000;
+for(var i = 1; i<stop; i++) {
+    addCollatz(i);
+}
+console.dir(mapping);
