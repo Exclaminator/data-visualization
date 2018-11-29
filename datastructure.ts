5d044a89244978007c238c0ef5d9e005a8014f83
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2018-11-29 11:46:28 +0100

    cleanup ts datastructure

--- datastructure.ts
+++ datastructure.ts
@@ -1,25 +1,3 @@
-class Collatz {
-
-}
-
-// class CollatzMap {
-//     private readonly mapping: Map<String, CollatzNode>;
-
-//     constructor() {
-//         this.mapping = new Map<String, CollatzNode>();
-//     }
-
-//     add(val: String, node: CollatzNode) {
-//         this.mapping.set(node.getValue(), node);
-//         return this;
-//     }
-
-//     get(val: String) {
-//         return this.mapping.get(val);
-//     }
-// }
-
-
 var mapping: Map<String, CollatzNode> = new Map<String, CollatzNode>();
 
 class CollatzNode {
@@ -62,9 +40,6 @@ function isEven(n) {
     return !(n & 1);
 }
 
-/**
- * @return Array of "step" values
- */
 function getPrev(n) {
     if (n == 1) {
         return 1;
@@ -115,42 +90,3 @@ function addCollatz(i: number) {
     mapping.set(i+"", node);
     return node;
 }
-
-// let medium = new StringMedium();
-// mapping.get("1").printUp(medium);
-// console.log(medium.read())
-addCollatz(20);
-addCollatz(3);
-addCollatz(13);
-addCollatz(80);
-addCollatz(17);
-addCollatz(104);
-
-// let medium = new StringMedium();
-// mapping.get("1").printUp(medium);
-// console.log(medium.read())
-
-console.dir(mapping.get("1"));
-
-// console.dir(mapping);
-// console.log("next::::");
-
-// medium = new StringMedium();
-// addCollatz(20).printUp(medium);
-// console.log(medium.read());
-
-// console.dir(mapping);
-
-// for(var i = 1; i < stop; i++){
-//     var v = i, v2;
-//     var node = new CollatzNode(i+""), node2, node3;
-//     node3 = node;
-//     do {
-//         mapping.set(v2+"", node2);
-//         v2 = getPrev(v);
-//         node2 = new CollatzNode(v2+"");
-//         node3.addToPrev(node2);
-//         node3 = node2;
-//     } while(!mapping.has(v2))
-//     node3.addToPrev(mapping.get(v2+""));
-//}
