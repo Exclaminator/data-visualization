2e8b591f38560aba87004d9cd01f3c19449d3f16
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2018-12-17 16:16:34 +0100

    refactor colorscheme into object

---
+++ ColorScheme.ts
@@ -0,0 +1,47 @@
+export interface ColorScheme {
+    forNumber(value: number): string;
+    forSpecial(): string;
+}
+
+export class ModuloColorScheme implements ColorScheme {
+    private readonly colorScheme = [
+        "#1f77b4",
+        "#aec7e8",
+        "#ff7f0e",
+        "#ffbb78",
+        "#2ca02c",
+        "#98df8a",
+        "#d62728",
+        "#ff9896",
+        "#9467bd",
+        "#c5b0d5",
+        "#8c564b",
+        "#c49c94",
+        "#e377c2",
+        "#f7b6d2",
+        "#7f7f7f",
+        "#c7c7c7",
+        "#bcbd22",
+        "#dbdb8d",
+        "#17becf",
+        "#9edae5"
+    ];
+
+    constructor(private readonly modulo: number) {
+        if (this.modulo - 1 > this.colorScheme.length) {
+            throw new Error(`ModuloColorScheme does not have enough colors to handle a modulo of this size. Available: ${this.colorScheme.length}, given modulo: ${this.modulo}`);
+        }
+    }
+
+    forNumber(value: number): string {
+        return this.colorScheme[value % this.modulo];
+    }
+
+    forSpecial(offset?: number): string {
+        if (!offset) {
+            offset = 0;
+        }
+
+        return this.colorScheme[this.modulo + offset];
+    }
+}
